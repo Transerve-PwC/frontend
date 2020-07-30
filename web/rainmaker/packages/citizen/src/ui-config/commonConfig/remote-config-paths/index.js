e0096bf10613ba90e58045172380799826619bd0
Tejaswi Katha <tejaswi.k@transerve.com>
2020-07-31 00:43:07 +0530

    conflicts resolved

--- web/rainmaker/packages/citizen/src/ui-config/commonConfig/remote-config-paths/index.js
+++ web/rainmaker/packages/citizen/src/ui-config/commonConfig/remote-config-paths/index.js
@@ -8,6 +8,7 @@ const remoteConfigPath = (path, screenKey) => {
     case "rented-properties": 
     case "rented-properties-citizen":
       config = require(`egov-rented-properties/ui-config/screens/specs/${path}/${screenKey}`).default;
+      break;
     case "estate":
       config = require(`egov-estate/ui-config/screens/specs/${path}/${screenKey}`).default;
       break;
