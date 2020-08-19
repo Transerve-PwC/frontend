0de73ab3b8fa4f9ee0e570e16e12072000db4987
Tejaswi Katha <tejaswi.k@transerve.com>
2020-08-19 21:42:51 +0530

    conflict resolved print menu

--- web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties/applyResource/footer.js
+++ web/rainmaker/dev-packages/egov-rented-properties-dev/src/ui-config/screens/specs/rented-properties/applyResource/footer.js
@@ -625,16 +625,6 @@ export const footer = getCommonApplyFooter({
       },
       leftIcon: "assignment"
     };
-    let applicationPrintObject = {
-      label: { labelName: "Application", labelKey: "TL_APPLICATION" },
-      link: () => {
-        const { Owners,OwnersTemp } = state.screenConfiguration.preparedFinalObject;
-        const documents = OwnersTemp[0].reviewDocData;
-        set(Owners[0],"additionalDetails.documents",documents)
-        downloadAcknowledgementForm(Owners, OwnersTemp[0].estimateCardData, "print");
-      },
-      leftIcon: "assignment"
-    };
     downloadMenu = [
       applicationDownloadObject
     ];
