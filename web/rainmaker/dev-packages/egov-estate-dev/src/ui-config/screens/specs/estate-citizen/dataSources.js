7b2f40f24a651a916c196a651495e64917b39bf2
Tejaswi Katha <tejaswi.k@transerve.com>
2020-09-08 22:15:31 +0530

    datasources

---
+++ web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate-citizen/dataSources.js
@@ -0,0 +1,61 @@
+import { getTenantId } from "egov-ui-kit/utils/localStorageUtils";
+import { httpRequest } from "../../../../ui-utils/api";
+
+var dataSources = {
+    "modesOfTransfer": {
+        name: "modesOfTransfer",
+        type: "local",
+        values: ["online", "offline"]
+    }
+}
+
+const getMdmsDataSourceValue = async (item) => {
+    if(!!item.values.cacheable && !!dataSources[item.name] && !!dataSources[item.name].values) {
+        return dataSources[item.name].values
+    } else {
+        const payload = {
+            MdmsCriteria: {
+                tenantId: getTenantId(),
+                moduleDetails: [
+                    {
+                        moduleName: item.values.moduleName,
+                        masterDetails: [
+                            {name: item.values.masterName, filter: item.values.masterName}
+                        ]
+                    }
+                ]
+            }
+        }
+        try {
+            const response = await httpRequest("post",
+            "/egov-mdms-service/v1/_search",
+            "_search",
+            [],
+            payload)
+            if(!!item.values.cacheable) {
+                dataSources[item.name] = {
+                    name: item.name,
+                    type: "mdms",
+                    values: response.MdmsRes[item.values.masterName]
+                }
+            }
+            return response.MdmsRes[item.values.masterName]
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+
+const getLocalDataSourceValue = (item) => {
+    return dataSources[item.name].values
+}
+
+
+export const getDataSourceValues = (item) => {
+    if(item.type === "local") {
+        return getLocalDataSourceValue(item);
+    } else {
+        return getMdmsDataSourceValue(item)
+    }
+}
