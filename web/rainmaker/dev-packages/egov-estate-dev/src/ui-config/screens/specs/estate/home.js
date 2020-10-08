1c7d0a21e67f6b35f4cc6d9e295862c915ef523a
Pooja Gawas <pooja.gawas@transerve.com>
2020-10-08 14:21:10 +0530

    added tenantId in allotment path

--- web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate/home.js
+++ web/rainmaker/dev-packages/egov-estate-dev/src/ui-config/screens/specs/estate/home.js
@@ -3,6 +3,9 @@ import {
   getCommonHeader
 } from "egov-ui-framework/ui-config/screens/specs/utils";
 import FormIcon from "../../../../ui-atoms-local/Icons/FormIcon";
+import {
+  getTenantId
+} from "egov-ui-kit/utils/localStorageUtils";
 
 const header = getCommonHeader({
   labelName: "Estate",
@@ -36,7 +39,7 @@ const cardItems = [
       labelName: "Allotment"
     },
     icon: < FormIcon / > ,
-    route: "allotment"
+    route: `allotment?tenantId=${getTenantId()}`
   },
   // {
   //   label: {
